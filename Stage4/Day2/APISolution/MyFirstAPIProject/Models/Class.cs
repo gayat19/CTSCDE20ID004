b58df4050fc2f5f5a48ab9a0651c7207e4dfb2e6
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-03-15 14:35:59 +0530

    Stage4Day4

---
+++ Stage4/Day2/APISolution/MyFirstAPIProject/Models/Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstAPIProject.Models
+{
+    public class Class1
+    {
+        public Class1(Employee employee)
+        {
+            Employee = employee;
+        }
+
+        public Employee Employee { get; }
+    }
+}
